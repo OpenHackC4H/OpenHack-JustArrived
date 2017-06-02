dc9f423d0ba5eb6894d170e9c0c6b255c95ea5a8
Leo Okawa Ericson <leo.ericson@yahoo.se>
2017-06-03 00:55:54 +0200

    Read JSON files for translation

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
 var express = require('express');
+var fs = require('fs');
 
 var router = new express.Router();
 
 router.get('/:lang/', function(req, res) {
     var lang = req.lang;
     var json;
+    fs.readFile(lang, function(err , data)){
+        if(err){
+            fs.readFile("en", function(err, data ){
+                json = JSON.parse(data);
+            });
+        }
+        else{
+            json = JSON.parse(data);
+        }
+    };
     res.render("index", json);
 });
 
