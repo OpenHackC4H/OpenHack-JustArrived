ded25bdddbaae52033a6eeda90aa574cb7a2aecc
Leo Okawa Ericson <leo.ericson@yahoo.se>
2017-06-03 02:16:18 +0200

    Add support for multiple languages

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,30 @@
 var express = require('express');
+var path = require('path');
 var fs = require('fs');
 
 var router = new express.Router();
 
-router.get('/:lang/', function(req, res) {
-    var lang = req.lang;
+router.get('/:lang/guide', function(req, res) {
+    var lang = req.params.lang;
     var json;
-    fs.readFile(lang, function(err , data)){
+    var filepath = path.join(__dirname, '../lang/' + lang + '.json');
+    fs.readFile(filepath, function(err , data){
         if(err){
-            fs.readFile("en", function(err, data ){
+            filepath = path.join(__dirname, '../lang/en.json');
+            fs.readFile(filepath, function(err, data ){
+                console.log("goddag " + err);
+                console.log("goddag " + data);
                 json = JSON.parse(data);
+                res.render("guide-template", json);
             });
         }
         else{
+            console.log("Hej " + err);
+            console.log("Hej " + data);
             json = JSON.parse(data);
+            res.render("guide-template", json);
         }
-    };
-    res.render("index", json);
+    });
 });
 
 module.exports = router;
